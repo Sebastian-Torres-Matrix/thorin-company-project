9be914e18176b7d78d498aa39852c388e0b8a907
Sebastian Torres <sebastian_torres@hotmail.se>
2020-05-21 18:45:51 +0000

    add config vars Heroku

--- run.py
+++ run.py
@@ -39,6 +39,6 @@ def careers():
     return render_template("careers.html", page_title="Career")
 
 if __name__ == '__main__':
-    app.run(host=os.environ.get('IP'),
-            port=int(os.environ.get('PORT')),
+    app.run(host=os.environ.get('IP', "0.0.0.0"),
+            port=int(os.environ.get('PORT', "5000")),
             debug=True)
